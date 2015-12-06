dcadd05a2bc86df0e0bd9d64caee27d30303d8d3
oeuillot <olivier.oeuillot@gmail.com>
2015-12-06 21:42:21 +0100

    Fix code

--- index.js
+++ index.js
@@ -6,22 +6,23 @@ var debug = require('debug')('freebox-jasmin:cli');
 var freebox = require('freebox-qml-run');
 var ip = require('ip');
 var Server = require('./lib/server');
+var ClientsList = require('./lib/clientsList');
 
 commander.version(require("./package.json").version);
 
-commander.option("--host <host>", "Freebox host");
-commander.option("--searchTimeout <milliseconds>",
-    "Freebox search timout in milliseconds", parseInt);
-
 commander.command('run').description("Run upnp jasmin server").action(
     function(programPath) {
-      var server = new Server(commander);
+
+      var clientsList = new ClientsList();
+
+      var server = new Server(commander, clientsList);
 
       server.listen(function(error) {
         if (error) {
           console.error(error);
           return;
         }
+
       });
     });
 
